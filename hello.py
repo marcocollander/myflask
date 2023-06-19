3aa6b70b0c532ae04b5a31b2d3449fea5356ebc6
Zenon Marek Raubuć <m.raubuc@wp.pl>
2023-06-19 06:51:27 +0200

    Added migration db

--- hello.py
+++ hello.py
@@ -1,12 +1,11 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -18,8 +17,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Role(db.Model):
@@ -36,6 +35,8 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
 
     def __repr__(self):
@@ -55,7 +56,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Zaloguj się")
 
 
-class RegistForm(FlaskForm):
+class RegisterForm(FlaskForm):
     name = StringField("Imię", validators=[DataRequired()])
     email = StringField("Email", validators=[Email()])
     password = PasswordField("Hasło", validators=[DataRequired()])
@@ -122,8 +123,8 @@ def login():
 
 
 @app.route("/reg", methods=["GET", "POST"])
-def regist():
-    form = RegistForm()
+def register():
+    form = RegisterForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
@@ -136,7 +137,7 @@ def regist():
         session["name"] = form.name.data
         return redirect(url_for("index"))
     return render_template(
-        "regist.html",
+        "register.html",
         form=form,
         name=session.get("name"),
         known=session.get("known"),
